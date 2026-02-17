0f2ce8417f84d5ba60aef92d2b8cc94b93feb92c
Clément.A <125081819+ITClmt@users.noreply.github.com>
2026-02-17 23:43:55 +0100

    feat(auth): implement refresh token-based authentication service and schema with a new `RefreshToken` model.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,39 @@ import { UsersService } from 'src/users/users.service';
 import * as argon2 from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login.dto';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UsersService, private readonly jwtService: JwtService) { }
+    constructor(private readonly userService: UsersService, private readonly jwtService: JwtService, private readonly configService: ConfigService, private readonly prisma: PrismaService) { }
+
+    private async generateAndSaveTokens(userId: string, email: string, role: string) {
+        const payload = { sub: userId, email, role };
+
+        const accessToken = await this.jwtService.signAsync(payload);
+
+        const refreshToken = await this.jwtService.signAsync(payload, {
+            secret: this.configService.getOrThrow<string>('JWT_REFRESH_SECRET'),
+            expiresIn: '7d',
+        });
+
+        const expiresAt = new Date();
+        expiresAt.setDate(expiresAt.getDate() + 7);
+
+        await this.prisma.refreshToken.create({
+            data: {
+                token: refreshToken,
+                userId: userId,
+                expiresAt: expiresAt,
+            }
+        });
+
+        return {
+            access_token: accessToken,
+            refresh_token: refreshToken,
+        };
+    }
 
     async login(loginDto: LoginDto) {
         const user = await this.userService.findByEmail(loginDto.email);
@@ -15,15 +44,7 @@ export class AuthService {
         const isPwdMatch = await argon2.verify(user.password, loginDto.password);
         if (!isPwdMatch) throw new UnauthorizedException('Email ou mot de passe incorrect');
 
-        const payload = {
-            sub: user.id,
-            email: user.email,
-            role: user.role
-        };
-
-        return {
-            access_token: await this.jwtService.signAsync(payload),
-        };
+        return this.generateAndSaveTokens(user.id, user.email, user.role);
     }
 
     async getProfile(id: string) {
